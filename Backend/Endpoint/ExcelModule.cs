26d5ab3ec8d915141e872bb7ce57750803d99340
siudeks <siudeks@gmail.com>
2017-05-05 23:07:05 +0200

    Add endpoint for simple extraction of status description

---
+++ Backend/Endpoint/ExcelModule.cs
@@ -0,0 +1,22 @@
+﻿using Backend;
+using Nancy;
+
+namespace Endpoint
+{
+    public sealed class ExcelModule : NancyModule
+    {
+        public ExcelModule()
+        {
+            Get("/excel/{id}/with/description", async _ =>
+            {
+                using (var gate = new VatPaymentChecker())
+                {
+                    await gate.Initialize();
+                    var result = await gate.Check(_.id);
+
+                    return result;
+                }
+            });
+        }
+    }
+}
--- Endpoint.Tests/EndpointShould.cs
+++ Endpoint.Tests/EndpointShould.cs
@@ -43,5 +43,24 @@ namespace Endpoint
             var result = response.Body.DeserializeJson<dynamic>();
             Assert.NotEmpty(result.Response);
         }
+
+        [Fact]
+        public async Task ShouldCheckWellKnownTaxPayerDescription()
+        {
+            var bootstrapper = new DefaultNancyBootstrapper();
+            var browser = new Browser(bootstrapper);
+
+            // When
+            var response = await browser.Get("/excel/5213017228/with/description", with =>
+            {
+                with.HttpRequest();
+                with.Accept(new Nancy.Responses.Negotiation.MediaRange("application/json"));
+            });
+
+            // Then
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            var result = response.Body.AsString();
+            Assert.NotEmpty(result);
+        }
     }
 }
