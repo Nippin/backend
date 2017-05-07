1da694bb4b50bafefaf788c8d51a22732aa9e40d
siudeks <siudeks@gmail.com>
2017-05-07 22:54:52 +0200

    Add simple message to home view

---
+++ Endpoint/HomeModule.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace Endpoint
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args =>
+            {
+                System.Console.WriteLine("Visit: / on " + System.Environment.MachineName);
+                return "Run excel and check ZUS status with function WEBSERVICE(\"http://nippin.cloudapp.net/excel/5213017228/with/description\")";
+            });
+        }
+    }
+}
