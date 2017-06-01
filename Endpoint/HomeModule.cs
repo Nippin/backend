cc55f649a55c77261f00d97a9cfca17f987c52fd
siudeks <siudeks@gmail.com>
2017-06-01 22:56:30 +0200

    Add better description on home page

--- Endpoint/HomeModule.cs
+++ Endpoint/HomeModule.cs
@@ -9,7 +9,10 @@ namespace Endpoint
             Get("/", args =>
             {
                 System.Console.WriteLine("Visit: / on " + System.Environment.MachineName);
-                return "Aby sprawdzić status płatnika VAT użyj funkcji excela: WEBSERVICE(\"http://nippin.cloudapp.net/excel/5213017228/with/description\") gdzie 5213017228 jest przykładowym sprawdzanym numerem NIP.";
+                return
+                    "Aby sprawdzić status płatnika VAT użyj funkcji excela: WEBSERVICE(\"http://nippin.cloudapp.net/excel/5213017228/with/description\") gdzie 5213017228 jest przykładowym sprawdzanym numerem NIP." +
+                    "\n\n" +
+                    "Aby pobrać weryfikację statusu płatnika VAT wywołaj http://nippin.cloudapp.net/api/screenshot/5213017228";
             });
         }
     }
