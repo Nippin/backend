bfe1430193c2d70dca9b69fce70f1b29aa8911b6
siudeks <siudeks@gmail.com>
2017-06-08 23:23:03 +0200

    Add CORS

--- Endpoint/AppBootstrapper.cs
+++ Endpoint/AppBootstrapper.cs
@@ -60,6 +60,18 @@ namespace Nippin
             environment.Tracing(enabled: false, displayErrorTraces: true);
             base.Configure(environment);
         }
-    }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            // CORS Enable
+            // https://stackoverflow.com/questions/15658627/is-it-possible-to-enable-cors-using-nancyfx
+            pipelines.AfterRequest.AddItemToEndOfPipeline((ctx) =>
+            {
+                ctx.Response.WithHeader("Access-Control-Allow-Origin", "*")
+                                .WithHeader("Access-Control-Allow-Methods", "POST,GET")
+                                .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+
+            });
+        }
 }
 
